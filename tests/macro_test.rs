f0fa71f6ec7d0201a0bacefea19befd49d66347a
Victor Sokolov <gzigzigzeo@gmail.com>
2024-12-10 20:21:19 +0100

    Converted tests to integration

---
+++ tests/macro_test.rs
@@ -0,0 +1,14 @@
+//! Those tests ensure that macro compiles
+
+// For now, now CI
+#[cfg(test)]
+mod test {
+    #[sqlx_pg_test_template::test]
+    async fn test_macro_default_custom(_pool: sqlx::Pool<sqlx::Postgres>) {}
+
+    #[sqlx_pg_test_template::test(max_connections = 5)]
+    async fn test_macro_default_custom_mc(_pool: sqlx::Pool<sqlx::Postgres>) {}
+
+    #[sqlx_pg_test_template::test(template = "postgres")]
+    async fn test_macro_default_custom_mc_tpl(_pool: sqlx::Pool<sqlx::Postgres>) {}
+}
